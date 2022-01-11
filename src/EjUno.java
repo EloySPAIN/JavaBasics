1f647e0b1945b8edd61764ff1c3ea392ae2aa27d
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-11 13:08:48 +0100

    Ejericios Tasca4

---
+++ src/EjUno.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Eloy Altozano Meijer
+ *
+ */
+public class EjUno {
+
+	public static void main(String[] args) {
+		int x=17;
+		int y=20;
+		int suma=x+y;
+		int resta=x-y;
+		int multi=x*y;
+		int div=x/y;
+		int mod=x%y;
+		System.out.println("Suma de "+x+"+"+ y+" es:"+suma);
+		System.out.println("Resta de "+x+"-"+ y+" es:"+resta);
+		System.out.println("Multiplicación de "+x+"*"+ y+" es:"+multi);
+		System.out.println("División de "+x+"/"+ y+" es:"+div);
+		System.out.println("Módulo de "+x+"%"+ y+" es:"+mod);
+	}
+
+}
