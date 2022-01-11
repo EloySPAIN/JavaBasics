c9655193ed067f2287d59824841ca1575d7649a6
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-11 13:25:52 +0100

    Ejercicio2

---
+++ src/EjDos.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Eloy Altozano Meijer
+ *
+ */
+public class EjDos {
+
+	public static void main(String[] args) {
+		int N=15;
+		double A=19.4;
+		char C= 'B';
+		System.out.println(N);
+		System.out.println(A);
+		System.out.println(C);
+		System.out.println("La suma de: "+N+"+"+A+" es: "+ (N+A));
+		System.out.println("La resta de: "+N+"-"+A+" es: "+ (N-A));
+		int chr=C;
+		System.out.println(chr);
+	}
+
+}
