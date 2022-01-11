f441a8608569e4b0bde5df51916b11bd78a15306
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-11 13:31:42 +0100

    Ejercicio3

--- src/EjTres.java
+++ src/EjTres.java
@@ -6,7 +6,11 @@
 public class EjTres {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int x=20;
+		int y=35;
+		double n=13.89;
+		double m=23.176;
+		
 
 	}
 
