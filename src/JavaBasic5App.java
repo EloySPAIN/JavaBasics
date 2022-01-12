5687e5411895bbb909a52e02264f962bef9e82da
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-12 08:10:48 +0100

    JavaBasic5App Eloy Altozano Meijer

--- src/JavaBasic5App.java
+++ src/JavaBasic5App.java
@@ -6,8 +6,17 @@
 public class JavaBasic5App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int A=6,B=15,C=7,D=24;
+		
+		System.out.println("A: "+A+" B: "+B+" C: "+C+" D: "+D);
+		B=C;
+		System.out.println("B=C: "+B);
+		C=A;
+		System.out.println("C=A: "+C);
+		A=D;
+		System.out.println("A=D: "+A);
+		D=B;
+		System.out.println("D=B: "+D);
 	}
 
 }
