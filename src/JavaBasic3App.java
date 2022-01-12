66dbf5d6a52d248e5bb3442c9c0b4f4fd51664dd
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-12 07:58:29 +0100

    JavaBasic3App

--- src/JavaBasic3App.java
+++ src/JavaBasic3App.java
@@ -3,15 +3,31 @@
  * @author Eloy Altozano Meijer
  *
  */
-public class EjTres {
+public class JavaBasic3App {
 
 	public static void main(String[] args) {
 		int x=20;
 		int y=35;
 		double n=13.89;
 		double m=23.176;
-		
-
+		System.out.println("La suma de: "+x+"+"+y+" es: "+ (x+y));
+		System.out.println("La resta de: "+x+"-"+y+" es: "+ (x-y));
+		System.out.println("El producto de: "+x+"*"+y+" es: "+ (x*y));
+		System.out.println("La suma de: "+x+"+"+y+" es: "+ (x+y));
+		System.out.println("El cociente de: "+x+"/"+y+" es: "+ (x/y));
+		System.out.println("La suma de: "+x+"+"+y+" es: "+ (x+y));
+		System.out.println("El resto de: "+x+"%"+y+" es: "+ (x%y+"\n"));
+		System.out.println("La suma de: "+n+"+"+m+" es: "+ (n+m));
+		System.out.println("La resta de: "+n+"-"+m+" es: "+ (n-m));
+		System.out.println("El producto de: "+n+"*"+m+" es: "+ (n*m));
+		System.out.println("El cociente de: "+n+"/"+m+" es: "+ (n/m));
+		System.out.println("El resto de: "+n+"$"+m+" es: "+ (n%m+"\n"));
+		System.out.println("La suma de: "+x+"+"+n+" es: "+ (x+n));
+		System.out.println("El cociente de: "+y+"/"+m+" es: "+ (y/m));
+		System.out.println("El resto de: "+y+"%"+m+" es: "+ (y%m));
+		System.out.println("El doble de: x"+x+" y"+y+" n"+n+" m"+m+"= x"+(x*2)+" y"+(y*2)+" n"+(n*2)+" m"+(m*2));
+		System.out.println("La suma de todas las variables: "+x+"+"+y+"+"+n+"+"+m+" es: "+ (x+y+n+m));
+		System.out.println("El producto de todas las variables: "+x+"*"+y+"*"+n+"*"+m+" es: "+ (x*y*n*m));
 	}
 
 }
