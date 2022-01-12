f2fb5793d0811957eae03af5496f0248cd04078c
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-01-12 08:04:58 +0100

    JavaBasic4App

--- src/JavaBasic4App.java
+++ src/JavaBasic4App.java
@@ -1,9 +1,17 @@
-
+/**
+ * 
+ * @author Eloy Altozano Meijer
+ *
+ */
 public class JavaBasic4App {
 
 	public static void main(String[] args) {
+		int N= 58;
 		
-
+		System.out.println("Valor inicial de N: "+N);
+		System.out.println("La suma de: "+N+" + 77 es: "+ (N+77));
+		System.out.println("La resta de: "+N+" - 3 es: "+ (N-3));
+		System.out.println("El producto de: "+N+" * 2 es: "+ (N*2));
 	}
 
 }
